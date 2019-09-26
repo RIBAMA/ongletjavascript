faecb2403aa3544a0c91713d29ecb9a2db50daad
destroyer <mougnolribama@gmail.com>
2019-09-26 14:00:05 +0100

    first test javascript: mesure of scope.

---
+++ app.js
@@ -0,0 +1,24 @@
+var parentContainerTabs = document.querySelectorAll(".parentContainerTab");
+
+
+(function(pcts){
+
+  //scope de la boucle de suppression de la classe .active
+  (function(){
+    for (var i = 0; i < pcts.length; i++) {
+      parentContainerTab = pcts[i];
+      (function(pct){
+        tabs = pct.querySelectorAll(".tabs");
+        console.log("Listes des tabs :" + i + " "+ tabs);
+        console.log(tabs);
+        (function(_tabs){
+          for (var i = 0; i < _tabs.length; i++) {
+            tab = _tabs[i];
+          }
+        })(tabs);
+      })(parentContainerTab);
+      // parentContainerTab.querySelector(".active").classList.remove("active");
+    }
+  })();
+
+})(parentContainerTabs);
